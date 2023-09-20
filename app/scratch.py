135ca9962ad9411177b69e46515af7cbf1378ae9
Alex Kravchenko <alex.kravchenko.me@gmail.com>
2023-09-20 21:06:19 +0200

    Keep stydying FastAPI

--- app/scratch.py
+++ app/scratch.py
@@ -1,18 +1,13 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
-
-
-class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
-
+"""Module allows to handle http requests."""
+from typing import Annotated
+from fastapi import FastAPI, Query
 
 app = FastAPI()
 
 
-@app.post("/items/")
-async def create_item(item: Item):
-    item.name + item.price
-    return item
+@app.get("/items/")
+async def read_items(q: Annotated[str | None, Query(max_length=50)] = None):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
